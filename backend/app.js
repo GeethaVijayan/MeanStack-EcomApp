4b18d614302a22d7634cd3a2af12191318122910
geethavijayan <gitavijayan30@gmail.com>
2026-08-26 19:23:17 +0530

    modified backend app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const customerRoutes = require('./routes/customer')
 const authRoutes = require('./routes/auth')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 // Parse JSON body
